ff51d001d587d610e9f1691fc5726a55581bf9b5
Sagore Sarker <57328825+sagoresarker@users.noreply.github.com>
2023-03-12 19:41:20 +0600

    Implemented pagination functionality

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from .models import Product
 from category.models import Category
 from carts.views import _cart_id
 from  carts.models import CartItem
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
 def store(request, category_slug=None):
@@ -12,14 +13,24 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.all().filter(category=categories, is_available=True)
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_product = paginator.get_page(page)
+
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_product = paginator.get_page(page)
+ 
+
         product_count = products.count()
 
     context = {
-        'products':products,
+        'products':paged_product,
         'product_count': product_count,
+        
     }
 
     return render(request, 'store/store.html', context)
