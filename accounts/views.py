15e861a26f4459325307c2bb214ca5f5faa0da7a
Sagore Sarker <57328825+sagoresarker@users.noreply.github.com>
2023-04-23 11:16:03 +0600

    implement Dynamically Redirect the User to Next Page

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from cart.models import Cart, CartItem
 from cart.views import _cart_id
+import requests
 
 #verification email
 from django.contrib.sites.shortcuts import get_current_site
@@ -110,7 +111,19 @@ def loginUser(request):
 
             login(request, user)
             messages.success(request, 'You are now logged in.')
-            return redirect('dashboard')
+
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+
+                # ?next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+
+                if 'next' in params:
+                    nextPage = params['next'] 
+                return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credientials.')
             return redirect('loginUser')
